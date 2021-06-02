2218655dc77c3b689d8103041c1083ee8306cc7a
anu1272 <anupamshrivastava7@gmail.com>
2021-06-02 03:51:16 +0100

    Movie application - initial stub

---
+++ src/common/header/Header.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+import './Header.css';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+
+class Header extends Component {
+
+
+    render(){
+        const access_token = "guest";
+        const book_show = "isMovieSelectedNot";
+        
+        let button;
+        let book_show_button;
+
+        const { classes } = this.props;
+
+        if(access_token=="guest"){
+            button = <Button variant="contained" onClick={this.handleOpen}>Login</Button>
+        }else{  
+            button = <Button variant="contained">Logout</Button>
+        }   
+
+        if(book_show=="isMovieSelected"){
+            book_show_button = <Button variant="contained" color="primary">Book Show</Button>
+        } 
+
+        return(
+            <div className="header">
+            <img className="logo" src="logo.svg" alt="_logo" />
+            <div className="book-show">
+                {book_show_button}
+            </div>
+            <div className="login-logout">
+                {button}
+            </div>
+
+         
+            
+        </div>
+        )
+    }
+
+    
+}
+
+
+export default Header;
