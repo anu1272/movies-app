310cde93d5fcf7a9a0b02fd780badc2b8a24f0b7
anu1272 <anupamshrivastava7@gmail.com>
2021-06-02 03:59:47 +0100

    Movie application - Header changes

---
+++ src/common/header/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+import './Header.css';
+
+class Header extends Component {
+
+
+    render(){
+        const access_token = "guest";
+        const book_show = "isMovieSelectedNot";
+        
+        let button;
+        let book_show_button;
+
+        const { classes } = this.props;
+
+        if(access_token=="guest"){
+            button = <Button variant="contained" onClick={this.handleOpen}>Login</Button>
+        }else{  
+            button = <Button variant="contained">Logout</Button>
+        }   
+
+        if(book_show=="isMovieSelected"){
+            book_show_button = <Button variant="contained" color="primary">Book Show</Button>
+        } 
+
+        return(
+            <div className="header">
+            <img className="logo" src="logo.svg" alt="_logo" />
+            <div className="book-show">
+                {book_show_button}
+            </div>
+            <div className="login-logout">
+                {button}
+            </div>
+
+         
+            
+        </div>
+        )
+    }
+
+    
+}
+
+
+export default Header;
